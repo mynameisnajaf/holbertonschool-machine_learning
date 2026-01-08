643d9fc8a5ba887af849b7cbde9fea2f82ba59cf
mynameisnajaf <12692@holbertonstudents.com>
2026-01-08 11:22:38 +0000

    \ 0-0 /

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""A module to rename the columns"""
+
+
+import pandas as pd
+
+
+def rename(df):
+    """Function that does the trick"""
+    df = df.rename(columns={"Timestamp":"Datetime"})
+    df["Datetime"] = pd.to_datetime(df["Datetime"])
+    df = df[["Datetime", "Close"]]
+
+    return df
