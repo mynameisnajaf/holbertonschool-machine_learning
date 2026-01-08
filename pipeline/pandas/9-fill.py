e945899c06a5badeb84864ecfc980b7d7e71084f
mynameisnajaf <12692@holbertonstudents.com>
2026-01-08 12:12:07 +0000

    \ 0-0 /

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+
+def fill(df):
+    """A function that does the trick"""
+    df = df.drop(columns=["Weighted_Price"])
+    df["Close"] = df["Close"].fillna(method="ffill")
+    for col in ["High", "Low", "Open"]:
+        df[col] = df[col].fillna(df["Close"]
+    for col in ["Volume_(BTC)", "Volume_(Currency)"]:
+        df[col] = df[col].fillna(0)
+    return df
