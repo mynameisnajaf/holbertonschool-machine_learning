da8103d81a4adbbe7d54a9786dbb4eff8af4c65b
mynameisnajaf <12692@holbertonstudents.com>
2026-01-08 13:10:42 +0000

    \ 0-0 /

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -11,8 +11,12 @@ def hierarchy(df1, df2):
     """A function that does the trick"""
     df1 = index(df1)
     df2 = index(df2)
-    df = pd.concat([df2.loc[1417411980:1417417980],
-                    df1.loc[1417411980:1417417980]],
-                    keys=["bitstamp","coinbase"])
+    df = pd.concat(
+        [
+            df2.loc[1417411980:1417417980],
+            df1.loc[1417411980:1417417980]
+        ],
+        keys=["bitstamp", "coinbase"]
+    )
     df = df.swaplevel(0, 1, axis=0)
-    return df.sort_index() 
+    return df.sort_index()
