241da7a92045ed4fc7edc40e00a0d7d62ca28446
mynameisnajaf <12692@holbertonstudents.com>
2026-01-08 12:42:24 +0000

    \ 0-0 /

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -11,4 +11,5 @@ def concat(df1, df2):
     """A function that does the trick"""
     df1 = index(df1)
     df2 = index(df2)
-    return pd.concat([df2.loc[:1417411920], df1], keys=["bitstamp", "coinbase"])
+    df2_selected = df2.loc[:1417411920]
+    return pd.concat([df2_selected, df1], keys=["bitstamp", "coinbase"])
