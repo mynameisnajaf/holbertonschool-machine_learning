0e08ca7fddd3b00683b5b22293f94885548791ff
mynameisnajaf <12692@holbertonstudents.com>
2026-01-08 12:36:40 +0000

    \ 0-0 /

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+
+def concat(df1, df2):
+    """A function that does the trick"""
+    index = __import__(&#39;10-index&#39;).index
+    df1 = index(df1)
+    df2 = index(df2)
+    return df.concat([df2.loc[:1417411920], df1], keys=["bitstamp", "coinbase"])
