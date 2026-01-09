9c4d29cb4efd56e562364bbdbae5d920ee335f98
mynameisnajaf <12692@holbertonstudents.com>
2026-01-09 15:22:57 +0400

    \ 0-0 /

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -10,7 +10,7 @@ def line():
     """A function to plot"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    x = np.arange(0,11)
+    x = np.arange(0, 11)
     plt.plot(x, y, color="red")
     plt.xlim(0, 10)
     plt.show()
