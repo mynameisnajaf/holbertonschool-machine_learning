ad59bac112ea7b63ee4398b734c01242c43fde8c
mynameisnajaf <12692@holbertonstudents.com>
2026-01-09 15:22:29 +0400

    \ 0-0 /

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+
+"""A  module that does the trick"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """A function to plot"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0,11)
