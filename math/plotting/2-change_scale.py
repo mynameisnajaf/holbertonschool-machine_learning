541b907deaeb0ea738aa1d83d7c3cc75e854ba56
mynameisnajaf <12692@holbertonstudents.com>
2026-01-09 15:36:24 +0400

    \ 0-0 /

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -14,7 +14,7 @@ def change_scale():
     y = np.exp((r / t) * x)
     plt.figure(figsize=(6.4, 4.8))
     plt.xlabel("Time (years)")
-    plt.ylabel("Fraction Remaining") 
+    plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of C-14")
     plt.yscale("log")
     plt.xlim(0, 28650)
