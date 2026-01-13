58d1a1cd8160ea0143f3f1669d77e41f24655fe7
mynameisnajaf <12692@holbertonstudents.com>
2026-01-13 11:55:57 +0000

    \ 0-0 /

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,10 +6,4 @@ def summation_i_squared(n):
     """Return the sum of squares from 1 to n."""
     if not isinstance(n, int) or n < 1:
         return None
-
-    total = 0
-    i = 1
-    while i <= n:
-        total += i ** 2
-        i += 1
-    return total
+    return n * (n + 1) * (2 * n + 1) // 6
