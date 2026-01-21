37b1307bb0c97deacc5dd8d56d7617f31882513e
mynameisnajaf <12692@holbertonstudents.com>
2026-01-21 08:39:19 +0000

    / 0-0 \

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+
+def cat_arrays(arr1, arr2):
+    """A function that does the trick"""
+    new_list = arr1 + arr2
+    return new_list
