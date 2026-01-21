59eefd1fbbbaa373d5533f74edcd610d1e9ed68b
mynameisnajaf <12692@holbertonstudents.com>
2026-01-21 09:46:47 +0000

    / 0-0 \

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """A function that does the trick"""
+    return np.matmul(mat1, mat2)
