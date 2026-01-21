8c58e07265cf8863a3c7acb27a1af4faf3cdc17f
mynameisnajaf <12692@holbertonstudents.com>
2026-01-21 09:41:46 +0000

    / 0-0 \

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+def np_elementwise(mat1, mat2):
+    """A function that does the trick"""
+    return mat1 + mat2, mat1 - mat2, mat1*mat2, mat1/mat2
