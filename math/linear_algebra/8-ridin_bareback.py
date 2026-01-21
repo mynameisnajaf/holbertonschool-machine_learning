88408fd49a17762f5fb50467b55e3fb497beb4c1
mynameisnajaf <12692@holbertonstudents.com>
2026-01-21 09:23:49 +0000

    / 0-0 \

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+
+def mat_mul(mat1, mat2):
+    """A function that does the trick"""
+    if len(mat1[0]) != len(mat2):
+       return None
+    new_matrix = []
+    for i in range(len(mat1)):
+        new_row = []
+        for j in range(len(mat2[0])):
+            sum = 0
+            for k in range(len(mat1[0])):
+                sum += mat1[i][k]*mat2[k][j]
+            new_row.append(sum)
+        new_matrix.append(new_row)
+    return new_matrix
