106ac47f654f11f0b954ea41cfbbc29175b37723
mynameisnajaf <12692@holbertonstudents.com>
2026-01-28 14:57:11 +0000

    / 0-0 \

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -13,8 +13,8 @@ def minor(matrix):
     for row in matrix:
         if type(row) is not list:
             raise TypeError("matrix must be a list of lists")
-        if len(row) != h:
-            raise ValueError("matrix must be a square matrix")
+        if len(row) != h or len(row) == 0:
+            raise ValueError("matrix must be a non-empty square matrix")
     for col in range(h):
         minor = [row[:col] + row[col+1:] for row in matrix[1:]]
         return minor
