3013e4dadf15af7d9d704178d04a4729449eea41
mynameisnajaf <12692@holbertonstudents.com>
2026-02-01 07:46:49 +0000

    / 0-0 \

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """A function that does the trick"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    transpose = np.transpose(matrix)
+    if not np.array_equal(transpose, matrix):
+        return None
+
+    eigen = np.linalg.eig(matrix)
+
+    if all(eigen > 0):
+        return 'Positive definite'
+    elif all(eigen >= 0):
+        return 'Positive semi-definite'
+    elif all(eigen < 0):
+        return 'Negative definite'
+    elif all(eigen <= 0):
+        return 'Negative semi-definite'
+    else:
+        return 'Indefinite'
