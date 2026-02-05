595c570bb3b059ad713ba7278d1d4a691314db4a
mynameisnajaf <12692@holbertonstudents.com>
2026-02-05 15:16:51 +0000

    / 0-0 \

---
+++ math/probability/poisson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+
+class Poisson:
+    """A class that does the trick"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """A function that does the trick"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = (sum(data) / len(data))
