16b892e8928355dff1fe4c1c64b4c933c7b84ea4
mynameisnajaf <12692@holbertonstudents.com>
2026-02-07 17:15:59 +0000

    / 0-0 \

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -35,3 +35,9 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        pi = 3.1415926536
+        e = 2.7182818285
+        """Calculates the PDF"""
+        return (1 / (self.stddev * ((2 * pi)**0.5))) * (e**(-(self.z_score(x))**2 / 2))
