5dbc978d08d471806660c48fa615e364e461ba58
mynameisnajaf <12692@holbertonstudents.com>
2026-02-07 18:06:25 +0000

    / 0-0 \

---
+++ math/probability/binomial.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+
+
+class Binomial:
+    """The class to call methods of binomial distribution"""
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """init"""
+        self.n = int(n)
+        self.p = float(p)
+        if data is None:
+            if n < 0:
+                raise ValueError("n must be a positive value")
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            summ = 0
+            for i in data:
+                summ += (i - self.mean) ** 2
+            variance = (summation / len(data))
+            self.p = 1 - (variance / mean)
+            self.n = int(round(mean / self.p))
+            self.p = mean / self.n
