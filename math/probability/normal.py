4bcb6b09028f9b577161f01b6eff3d28cd4c44c4
mynameisnajaf <12692@holbertonstudents.com>
2026-02-07 16:54:51 +0000

    / 0-0 \

---
+++ math/probability/normal.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+import numpy as np
+
+
+class Normal:
+    """
+    Tye class to call methods of Normal distribution
+    """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """A function that does the trick"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.mean = (sum(data) / len(data))
+                summ = 0
+                for i in data:
+                    summ += (i - self.mean) ** 2
+
+                self.stddev = (summ / len(data)) ** 0.5
