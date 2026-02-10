a7253f199043fed5084a3b6bd138ac551dda229a
mynameisnajaf <12692@holbertonstudents.com>
2026-02-10 14:05:00 +0400

    / 0-0 \

---
+++ math/bayesian_prob/holberton.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""A module that does the trick"""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """A function that does the trick"""
+    if n < 0:
+        raise ValueError('n must be a positive integer')
+    if type(x) is not int and x < 0:
+        raise ValueError('x must be an integer that is greater than or equal to 0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    factorial = np.math.factorial
+    numerator = factorial(n)
+    denominator = factorial(x) * factorial(n - x)
+    factor = numerator / denominator
+    p_likelihood = factor * (np.power(P, x)) * (np.power((1 - P), (n - x)))
+    return p_likelihood
