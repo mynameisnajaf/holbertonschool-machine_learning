0e7121cbd677a58397af17c8630c9e94a5b774fa
mynameisnajaf <12692@holbertonstudents.com>
2026-02-18 18:39:49 +0400

    / 0-0 \

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -50,14 +50,16 @@ class Node:
 
     def right_child_add_prefix(self, text):
         lines = text.split("\n")
-        new_text = "    └-- " + lines[0]
+        new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += "\n        " + x
-        return (new_text)
+            new_text += ("       " + x) + "\n"
+        return new_text
 
     def __str__(self):
-        """Return a string representation of the decision tree"""
-        text = f"[feature={self.feature}, threshold={self.threshold}]"
+        if self.is_root:
+            text = f"root [feature={self.feature}, threshold={self.threshold}]"
+        else:
+            text = f"node [feature={self.feature}, threshold={self.threshold}]"
 
         if self.left_child:
             left_text = str(self.left_child)
@@ -126,36 +128,3 @@ class Decision_Tree():
     def __str__(self):
         """Return a string representation of the decision tree"""
         return self.root.__str__()
-
-def example_0():
-    leaf0 = Leaf(0, depth=1)
-    leaf1 = Leaf(0, depth=2)
-    leaf2 = Leaf(1, depth=2)
-    internal_node = Node(feature=1, threshold=30000, left_child=leaf1, right_child=leaf2, depth=1)
-    root = Node(feature=0, threshold=.5, left_child=leaf0, right_child=internal_node, depth=0, is_root=True)
-    return Decision_Tree(root=root)
-
-
-def example_1(depth):
-    level = [Leaf(i, depth=depth) for i in range(2 ** depth)]
-    level.reverse()
-
-    def get_v(node):
-        if node.is_leaf:
-            return node.value
-        else:
-            return node.threshold
-
-    for d in range(depth):
-        level = [Node(feature=0,
-                      threshold=(get_v(level[2 * i]) + get_v(level[2 * i + 1])) / 2,
-                      left_child=level[2 * i],
-                      right_child=level[2 * i + 1], depth=depth - d - 1) for i in range(2 ** (depth - d - 1))]
-    root = level[0]
-    root.is_root = True
-    return Decision_Tree(root=root)
-
-#Print Tree example 0
-print(example_0())
-#Print Tree example 1
-print(example_1(4))
