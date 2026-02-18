9e40d3b65a5633ed11237435fab559657bd2a2d8
mynameisnajaf <12692@holbertonstudents.com>
2026-02-18 20:35:01 +0400

    / 0-0 \

---
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+"""A module to build a tree"""
+import numpy as np
+
+
+class Node:
+    """A class to represent a node in a decision tree"""
+
+    def __init__(self,
+                 feature=None,
+                 threshold=None,
+                 left_child=None,
+                 right_child=None,
+                 is_root=False, depth=0):
+        """Constructor method"""
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Return the maximum depth of the decision tree"""
+        if self.is_leaf:
+            return self.depth
+        else:
+            return max(
+                self.left_child.max_depth_below(),
+                self.right_child.max_depth_below()
+            )
+
+    def count_nodes_below(self, only_leaves=False):
+        """Return the number of nodes in the decision tree"""
+        left = self.left_child.count_nodes_below(only_leaves=only_leaves)
+        right = self.right_child.count_nodes_below(only_leaves=only_leaves)
+        if only_leaves:
+            return left + right
+        else:
+            return 1 + left + right
+
+    def __str__(self):
+        """Return a string representation of the decision tree"""
+        if self.is_root:
+            prefix = "root"
+        else:
+            prefix = "-> node"
+
+        result = (f"{prefix} [feature={self.feature}, "
+                  f"threshold={self.threshold}]\n")
+        result += self.left_child_add_prefix(self.left_child.__str__())
+        result += self.right_child_add_prefix(self.right_child.__str__())
+        return result
+
+    def left_child_add_prefix(self, text):
+        """Add a left child prefix to the decision tree"""
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
+            if x.strip():
+                new_text += ("    |  " + x) + "\n"
+        return (new_text)
+
+    def right_child_add_prefix(self, text):
+        """Add a right child prefix to the decision tree"""
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
+            if x.strip():
+                new_text += ("       " + x) + "\n"
+        return (new_text)
+
+    def get_leaves_below(self):
+        """Return leaves below the decision tree"""
+        leaves = []
+        if self.left_child:
+            leaves.extend(self.left_child.get_leaves_below())
+        if self.right_child:
+            leaves.extend(self.right_child.get_leaves_below())
+        return leaves
+
+    def update_bounds_below(self):
+        """Update the bounds below the decision tree"""
+        if self.is_root:
+            self.upper = {0: np.inf}
+            self.lower = {0: -1*np.inf}
+
+        for child in [self.left_child, self.right_child]:
+            if child is None:
+                continue
+
+            child.upper = self.upper.copy()
+            child.lower = self.lower.copy()
+            if child == self.left_child:
+                child.lower[self.feature] = self.threshold
+            else:
+                child.upper[self.feature] = self.threshold
+
+        for child in [self.left_child, self.right_child]:
+            child.update_bounds_below()
+
+    def update_indicator(self):
+        """Update the indicator for the decision tree"""
+
+        def is_large_enough(x):
+            """Return True if x is larger than threshold"""
+            for feature in self.lower:
+                return (x[:, feature] > self.lower[feature])
+
+        def is_small_enough(x):
+            """Return True if x is smaller than threshold"""
+            for feature in self.lower:
+                return (x[:, feature] <= self.upper[feature])
+        self.indicator = lambda x: np.all(np.array([is_large_enough(x), is_small_enough(x)]), axis=0)
+
+
+class Leaf(Node):
+    """A class to represent a leaf in a decision tree"""
+
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Return the maximum depth of the decision tree"""
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """Return the number of nodes in the decision tree"""
+        return 1
+
+    def __str__(self):
+        """Return a string representation of the decision tree"""
+        return (f"-> leaf [value={self.value}]")
+
+    def get_leaves_below(self):
+        """Return leaves below the decision tree"""
+        return [self]
+
+    def update_bounds_below(self):
+        """Update the bounds below the decision tree"""
+        pass
+
+
+class Decision_Tree():
+    """A class to represent a decision tree"""
+
+    def __init__(self,
+                 max_depth=10,
+                 min_pop=1,
+                 seed=0,
+                 split_criterion="random",
+                 root=None
+                 ):
+        """Constructor method"""
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Return the depth of the decision tree"""
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        """Return the number of nodes in the decision tree"""
+        return self.root.count_nodes_below(only_leaves=only_leaves)
+
+    def __str__(self):
+        """Return a string representation of the decision tree"""
+        return self.root.__str__()
+
+    def get_leaves(self):
+        """Return leaves below the decision tree"""
+        return self.root.get_leaves_below()
+
+    def update_bounds(self):
+        """Update the bounds below the decision tree"""
+        self.root.update_bounds_below()
+
+def example_0():
+    leaf0 = Leaf(0, depth=1)
+    leaf1 = Leaf(0, depth=2)
+    leaf2 = Leaf(1, depth=2)
+    internal_node = Node(feature=1, threshold=30000, left_child=leaf1, right_child=leaf2, depth=1)
+    root = Node(feature=0, threshold=.5, left_child=leaf0, right_child=internal_node, depth=0, is_root=True)
+    return Decision_Tree(root=root)
