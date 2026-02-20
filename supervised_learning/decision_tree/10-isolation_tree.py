36cca210675da7889f4108f7fb2ebd3d51ffd199
mynameisnajaf <12692@holbertonstudents.com>
2026-02-20 16:09:57 +0400

    / 0-0 \

--- supervised_learning/decision_tree/10-isolation_tree.py
+++ supervised_learning/decision_tree/10-isolation_tree.py
@@ -5,19 +5,21 @@ Node = __import__('8-build_decision_tree').Node
 Leaf = __import__('8-build_decision_tree').Leaf
 
 
-class Isolation_Random_Tree():
+class Isolation_Random_Tree:
     """Isolation Random Tree"""
+
     def __init__(self, max_depth=10, seed=0, root=None):
-        """Heyyo"""
+        """Constructor"""
         self.rng = np.random.default_rng(seed)
+        self.max_depth = max_depth
+        self.predict = None
+        self.min_pop = 1
+        self.explanatory = None
+
         if root:
             self.root = root
         else:
             self.root = Node(is_root=True)
-        self.explanatory = None
-        self.max_depth = max_depth
-        self.predict = None
-        self.min_pop = 1
 
     def __str__(self):
         """String representation"""
@@ -40,7 +42,7 @@ class Isolation_Random_Tree():
         return self.root.get_leaves_below()
 
     def update_predict(self):
-        """Update the prediction of the tree"""
+        """Update the prediction function"""
         self.update_bounds()
         leaves = self.get_leaves()
         for leaf in leaves:
@@ -51,7 +53,7 @@ class Isolation_Random_Tree():
         )
 
     def np_extrema(self, arr):
-        """Extrema of the extrema of the array"""
+        """Return min and max of array"""
         return np.min(arr), np.max(arr)
 
     def random_split_criterion(self, node):
@@ -68,65 +70,64 @@ class Isolation_Random_Tree():
         return feature, threshold
 
     def get_leaf_child(self, node, sub_population):
+        """Return a leaf node"""
         leaf_child = Leaf(value=node.depth + 1)
         leaf_child.depth = node.depth + 1
         leaf_child.sub_population = sub_population
         return leaf_child
 
     def get_node_child(self, node, sub_population):
-        """Get the child node of the tree"""
+        """Return a regular node"""
         n = Node()
         n.depth = node.depth + 1
         n.sub_population = sub_population
         return n
 
     def fit_node(self, node):
-        """Fits node"""
+        """Recursively fit nodes"""
         node.feature, node.threshold = self.random_split_criterion(node)
 
         left_population = node.sub_population & (
-                self.explanatory[:, node.feature] > node.threshold
+            self.explanatory[:, node.feature] > node.threshold
         )
         right_population = node.sub_population & (
-                self.explanatory[:, node.feature] <= node.threshold
+            self.explanatory[:, node.feature] <= node.threshold
         )
 
-        # Is left node a leaf ?
+        # Is left node a leaf?
         is_left_leaf = (
-                node.depth + 1 >= self.max_depth or
-                np.sum(left_population) <= self.min_pop
+            node.depth + 1 >= self.max_depth or
+            np.sum(left_population) <= self.min_pop
         )
 
         if is_left_leaf:
-            node.left_child = self.get_leaf_child(node,left_population)
+            node.left_child = self.get_leaf_child(node, left_population)
         else:
-            node.left_child = self.get_node_child(node,left_population)
+            node.left_child = self.get_node_child(node, left_population)
             self.fit_node(node.left_child)
 
-        # Is right node a leaf ?
+        # Is right node a leaf?
         is_right_leaf = (
-                node.depth + 1 >= self.max_depth or
-                np.sum(right_population) <= self.min_pop
+            node.depth + 1 >= self.max_depth or
+            np.sum(right_population) <= self.min_pop
         )
 
         if is_right_leaf:
-            node.right_child = self.get_leaf_child(node,right_population)
+            node.right_child = self.get_leaf_child(node, right_population)
         else:
-            node.right_child = self.get_node_child(node,right_population)
+            node.right_child = self.get_node_child(node, right_population)
             self.fit_node(node.right_child)
 
-
-    def fit(self, explanatory,verbose=0):
-
-        self.split_criterion = self.random_split_criterion
+    def fit(self, explanatory, verbose=0):
+        """Fit the isolation tree"""
         self.explanatory = explanatory
-        self.root.sub_population = np.ones_like(explanatory.shape[0], dtype='bool')
+        self.root.sub_population = np.ones(explanatory.shape[0], dtype=bool)
 
         self.fit_node(self.root)
         self.update_predict()
 
         if verbose == 1:
             print(f"""  Training finished.
-    - Depth                     : { self.depth()       }
-    - Number of nodes           : { self.count_nodes() }
-    - Number of leaves          : { self.count_nodes(only_leaves=True) }""")
+    - Depth                     : {self.depth()}
+    - Number of nodes           : {self.count_nodes()}
+    - Number of leaves          : {self.count_nodes(only_leaves=True)}""")
