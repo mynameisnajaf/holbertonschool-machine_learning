d42c259090345f25132ebe1de4d1aea583899a5c
mynameisnajaf <12692@holbertonstudents.com>
2026-03-04 18:34:42 +0400

    / 0-0 \

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import numpy as np
+
+
+class Neuron:
+    """A class that does the trick"""
+
+    def __init__(self, nx):
+        """Initialize the class"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Return the weight matrix"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Return the bias vector"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Return the activation matrix"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Forward propagation method"""
+        z = np.matmul(self.W, X) + self.b
+        self.__A = 1 / (1 + (np.exp(-z)))
+        return self.A
+
+    def cost(self, Y, A):
+        """Return the cost function"""
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-m_loss)
+        return cost
