d6f878aa6cc477da29659ebf4c60e9f4fe302745
mynameisnajaf <12692@holbertonstudents.com>
2026-03-07 21:09:53 +0400

    / 0-0 \

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """A class that does the trick"""
+
+    def __init__(self, nx, layers):
+        """Constructor"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for lay in range(self.__L):
+            if layers[lay] < 1 or not isinstance(layers[lay], int):
+                raise TypeError("layers must be a list of positive integers")
+
+            if lay == 0:
+                prev = nx
+            else:
+                prev = layers[lay - 1]
+            self.__weights['W{}'.format(lay + 1)] = (
+                np.random.randn(layers[lay], prev) * np.sqrt(2 / prev)
+            )
+
+            self.__weights['b{}'.format(lay + 1)] = np.zeros((layers[lay], 1))
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def layers(self):
+        return self.__L
