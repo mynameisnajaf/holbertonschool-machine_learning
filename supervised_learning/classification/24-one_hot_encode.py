cc576f9cbf247656196f07641bd035b3034de896
mynameisnajaf <12692@holbertonstudents.com>
2026-03-07 22:05:56 +0400

    / 0-0 \

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -4,6 +4,15 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """One-hot encode Y"""
+    if len(Y) == 0:
+        return None
+    elif type(classes) is not int:
+        return None
+    elif not isinstance(Y, np.ndarray):
+        return None
+    elif classes <= np.amax(Y):
+        return None
+
     ohe = np.zeros((classes, len(Y)))
     ohe[Y, np.arange(len(Y))] = 1
     return ohe
