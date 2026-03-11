545ff53961b3c89d41de1073b2003466578c5a12
mynameisnajaf <12692@holbertonstudents.com>
2026-03-11 18:14:02 +0400

    / 0-0 \

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -5,7 +5,7 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size,
-                epochs, validation_data = None, verbose=True, shuffle=False):
+                epochs, validation_data=None, verbose=True, shuffle=False):
     """Train model"""
     history = network.fit(
         data,
