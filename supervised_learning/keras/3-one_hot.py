b1af3f60ae553e3dd388ef0695052af03c46e782
mynameisnajaf <12692@holbertonstudents.com>
2026-03-11 17:30:49 +0400

    / 0-0 \

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -6,14 +6,6 @@ import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
     """One-hot encoding of labels"""
-    if len(labels) == 0:
-        return None
-    elif type(classes) is not int:
-        return None
-    elif not isinstance(labels, np.ndarray):
-        return None
-    elif classes <= np.amax(labels):
-        return None
 
     ohe = np.zeros((classes, len(labels)))
     ohe[labels, np.arange(len(labels))] = 1
