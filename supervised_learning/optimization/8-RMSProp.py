12acf135e336f9952a712c005269f2aa418a9881
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 17:54:24 +0400

    / 0-0 \

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def create_RMSProp_op(alpha, beta2, epsilon):
     """Creates a RMSProp operation"""
     optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha,
-                                            weight_decay=beta2,
-                                            momentum=epsilon)
+                                            rho=beta2,
+                                            epsilon=epsilon)
     return optimizer
