b50344e61ad013451a39ab067525ee6038cec1f5
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 18:36:04 +0400

    / 0-0 \

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -5,5 +5,5 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """The learning rate decay"""
-    lr = alpha / (1 + decay_rate * ( global_step // decay_step))
+    lr = alpha / (1 + decay_rate * (global_step // decay_step))
     return lr
