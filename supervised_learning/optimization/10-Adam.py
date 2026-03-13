fe484c7a8aea5c80c8d5a55fde887816fbbbac98
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 18:30:54 +0400

    / 0-0 \

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def create_Adam_op(alpha, beta1, beta2, epsilon):
     """Creates an Adam optimizer"""
     optimizer = tf.keras.optimizers.Adam(learning_rate=alpha,
-                                         beta1=beta1,
-                                         beta2=beta2,
+                                         beta_1=beta1,
+                                         beta_2=beta2,
                                          epsilon=epsilon)
     return optimizer
