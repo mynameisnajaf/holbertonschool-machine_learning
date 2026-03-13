dbe24eab050ae6c9ed4efe2d73e714a9fd325475
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 17:33:09 +0400

    / 0-0 \

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def moving_average(data, beta):
+    """Moving Average"""
+    vt = 0
+    moving_avg = []
+    for t, x in enumerate(data):
+        vt = vt * beta + (1 - beta) * x
+        bias = 1 - (beta ** (t + 1))
+        vt_corrected = vt / bias
+        moving_avg.append(vt_corrected)
+
+    return moving_avg
