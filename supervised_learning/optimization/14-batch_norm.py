780142f45329cc5401761a3c3104c9907c86698c
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 18:57:26 +0400

    / 0-0 \

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 
 def create_batch_norm_layer(prev, n, activation):
     """Creates a batch normalization layer"""
-    activation = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    active = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.keras.layers.Dense(n, activation=None,
-                            kernel_initializer=activation, name='layer')(prev)
+                            kernel_initializer=active, name='layer')(prev)
     mu, sigma_2 = tf.nn.moments(layer, axes=[0])
     gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]),
                         name='gamma')
