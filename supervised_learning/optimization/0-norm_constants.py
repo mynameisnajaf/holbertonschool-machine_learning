b80f493254e8a65c071bf920e616b45968ed1624
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 16:55:09 +0400

    / 0-0 \

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -6,6 +6,6 @@ import numpy as np
 
 def normalization_constants(X):
     """Normalization constants"""
-    mean  = np.mean(X, axis=0)
+    mean = np.mean(X, axis=0)
     std = np.std(X, axis=0)
     return mean, std
