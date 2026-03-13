20cb368958c21f33c0fade94cd304ada0d3c1734
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 18:26:44 +0400

    / 0-0 \

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -6,6 +6,7 @@ import numpy as np
 def update_variables_Adam(alpha, beta1,
                           beta2, epsilon,
                           var, grad, v, s, t):
+    """Adam hates Eve"""
     vt = beta1 * v + (1 - beta1) * grad
     st = beta2 * s + (1 - beta2) * grad ** 2
     vt_hat = vt / (1 - beta1 ** t)
