601cb7685fa29214e2a240118f0490f2b2e3dd2d
mynameisnajaf <12692@holbertonstudents.com>
2026-03-13 17:48:43 +0400

    / 0-0 \

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Updates the RMSProp variable"""
-    vt = beta2 * var + (1 - beta2) * grad ** 2
-    var = s - alpha * grad / (np.sqrt(vt + epsilon))
-    return var
+    vt = beta2 * s + (1 - beta2) * grad ** 2
+    rms = var - alpha * grad / (np.sqrt(vt + epsilon))
+    return rms, vt
