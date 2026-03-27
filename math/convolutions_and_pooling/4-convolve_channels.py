d1fd921572ebe72141e577963f14f595ffcc79eb
mynameisnajaf <12692@holbertonstudents.com>
2026-03-28 00:07:36 +0400

    / 0-0 \

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """convolve the images using a kernel"""
     m, h, w, c = images.shape
     kh, kw = kernel.shape
