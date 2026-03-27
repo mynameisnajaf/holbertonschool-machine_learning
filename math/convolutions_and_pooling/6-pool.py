c01db68c9dc0afd98eb680d5fa23d55eacfa271a
mynameisnajaf <12692@holbertonstudents.com>
2026-03-28 00:20:10 +0400

    / 0-0 \

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""A module that does the trick"""
+"""Performs pooling on images"""
 import numpy as np
 
 
 def pool(images, kernel_shape, stride, mode='max'):
-    """convolve the images using a kernel"""
+    """performs max or average pooling on images"""
     m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
-    output_h = int(1 + ((h - kh) / sh))
-    output_w = int(1 + ((w - kw) / sw))
+    output_h = int(1 + (h - kh) / sh)
+    output_w = int(1 + (w - kw) / sw)
 
-    convolve = np.zeros((m, output_h, output_w, c))
-    image = np.arange(m)
+    output = np.zeros((m, output_h, output_w, c))
 
     for x in range(output_h):
         for y in range(output_w):
+            region = images[
+                :,
+                x * sh:(x * sh) + kh,
+                y * sw:(y * sw) + kw,
+                :
+            ]
+
             if mode == 'max':
-                convolve[image, x, y] = (np.max(images[image,
-                x * sh:((x * sh) + kh),
-                y * sw:((y * sw) + kw)],
-                                                axis=(1, 2)))
+                output[:, x, y, :] = np.max(region, axis=(1, 2))
             elif mode == 'avg':
-                convolve[image, x, y] = (np.mean(images[image,
-                x * sh:((x * sh) + kh),
-                y * sw:((y * sw) + kw)],
-                                                 axis=(1, 2)))
-    return convolve
+                output[:, x, y, :] = np.mean(region, axis=(1, 2))
+
+    return output
