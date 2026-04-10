bb5d098f9488fb9b8b308238383eb419e9c62a50
mynameisnajaf <12692@holbertonstudents.com>
2026-04-11 01:49:55 +0400

    / 0-0 \

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -5,42 +5,7 @@ import tensorflow as tf
 
 def pca_color(image, alphas):
     """PCA color augmentation using TensorFlow only"""
+    image_nparray = tf.keras.preprocessing.image.img_to_array(image)
+    img = image_nparray.reshape(-1, 3).astype(float)
 
-    # Convert to float32 (DO NOT normalize)
-    image = tf.cast(image, tf.float32)
-
-    # Reshape to (num_pixels, 3)
-    flat = tf.reshape(image, [-1, 3])
-
-    # Center the data
-    mean = tf.reduce_mean(flat, axis=0)
-    centered = flat - mean
-
-    # Covariance matrix
-    cov = tf.matmul(centered, centered, transpose_a=True)
-    cov /= tf.cast(tf.shape(flat)[0] - 1, tf.float32)
-
-    # Eigen decomposition
-    eigvals, eigvecs = tf.linalg.eigh(cov)
-
-    # Sort descending
-    idx = tf.argsort(eigvals, direction='DESCENDING')
-    eigvals = tf.gather(eigvals, idx)
-    eigvecs = tf.gather(eigvecs, idx, axis=1)
-
-    alphas = tf.cast(alphas, tf.float32)
-    delta = tf.matmul(
-        eigvecs,
-        tf.reshape(alphas * tf.sqrt(eigvals), [-1, 1])
-    )
-
-    # Add PCA noise
-    flat_aug = flat + tf.reshape(delta, [1, 3])
-
-    # Reshape back
-    augmented = tf.reshape(flat_aug, tf.shape(image))
-
-    # Clip and return
-    augmented = tf.clip_by_value(augmented, 0, 255)
-
-    return tf.cast(augmented, tf.uint8)
+    return img
