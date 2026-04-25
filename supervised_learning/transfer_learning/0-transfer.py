01bcfe325e41fd58f52530d95fb35c69df638661
mynameisnajaf <12692@holbertonstudents.com>
2026-04-25 22:10:01 +0400

    / 0-0 \

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -21,10 +21,13 @@ if __name__ == '__main__':
 
     input_K = K.layers.Input(shape=(32, 32, 3))
     input_resize = K.layers.Lambda(
-        lambda x: K.backend.resize_images(x,
-                                          height=(224 // 32),
-                                          width=(224 // 32),
-                                          data_format="channels_last")
+        lambda x: K.backend.resize_images(
+            x,
+            height_factor=(224 // 32),
+            width_factor=(224 // 32),
+            data_format="channels_last"
+        ),
+        output_shape=(224, 224, 3)
     )(input_K)
 
     InceptionV3 = K.applications.inception_v3.InceptionV3(include_top=False,
@@ -34,7 +37,7 @@ if __name__ == '__main__':
     activation = K.activations.relu
 
     X = InceptionV3(input_resize, training=False)
-    X = K.layers.Flatten()(X)
+    X = K.layers.GlobalAvgPool2D()(X)
     X = K.layers.Dense(500, activation=activation)(X)
     X = K.layers.Dropout(0.2)(X)
     output_K = K.layers.Dense(10, activation='softmax')(X)
@@ -48,6 +51,6 @@ if __name__ == '__main__':
                   )
     history = model.fit(x=X_train, y=Y_train,
                         validation_data=(X_test, Y_test),
-                        batch_size=300,
+                        batch_size=64,
                         epochs=5, verbose=True)
     model.save('cifar10.h5')
