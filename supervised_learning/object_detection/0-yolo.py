0e57d9dcb952fab5035ecde863fc4e831ccc1f03
mynameisnajaf <12692@holbertonstudents.com>
2026-05-03 00:28:00 +0400

    / 0-0 \

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -7,7 +7,7 @@ class Yolo:
     """A class that does the trick"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Initializer for the class"""
-        self.model_path = tf.keras.models.load_model(model_path)
+        self.model = tf.keras.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = [line.strip() for line in f]
         self.class_t = class_t
