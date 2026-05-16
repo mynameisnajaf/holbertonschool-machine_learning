d59945653f871abe470f9d04ecedf5a67367707e
mynameisnajaf <12692@holbertonstudents.com>
2026-05-17 01:00:50 +0400

    / 0-0 \

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def P_init(X, perplexity):
     """A function that does the trick"""
-    n , d = X.shape
+    n, d = X.shape
     sum_X = np.sum(np.square(X), axis=1)
     D = (np.add(np.add(-2 * np.matmul(X, X.T), sum_X).T, sum_X))
     np.fill_diagonal(D, 0)
