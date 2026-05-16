e349284b50f3832cd84b638fe6c93437611ccb11
mynameisnajaf <12692@holbertonstudents.com>
2026-05-17 00:41:05 +0400

    / 0-0 \

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """A function that does the trick"""
+    U, sigma, V = np.linalg.svd(X)
+    explained_variance = (sigma ** 2) / np.sum(sigma ** 2)
+    cumulative_variance = np.cumsum(explained_variance)
+    nd = np.where(cumulative_variance > var)[0][0] + 1
+    W = U.T[:, :nd]
+
+    return W
---
+++ unsupervised_learning/dimensionality_reduction/README.md
@@ -0,0 +1,1 @@
+What is dimensionality reduction?
