e0e6d565ae06ec5990731fa913a1a6d0af9d955a
mynameisnajaf <12692@holbertonstudents.com>
2026-05-17 00:53:49 +0400

    / 0-0 \

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -5,10 +5,9 @@ import numpy as np
 
 def pca(X, var=0.95):
     """A function that does the trick"""
-    U, sigma, V = np.linalg.svd(X)
-    explained_variance = (sigma ** 2) / np.sum(sigma ** 2)
-    cumulative_variance = np.cumsum(explained_variance)
-    nd = np.where(cumulative_variance >= var)[0][0] + 1
-    W = V.T[:, :nd]
-
-    return W
+    u, Sigma, vh = np.linalg.svd(X, full_matrices=False)
+    cumulative_var = np.cumsum(Sigma) / np.sum(Sigma)
+    r = (np.argwhere(cumulative_var >= var))[0, 0]
+    w = vh.T
+    wr = w[:, :r + 1]
+    return wr
