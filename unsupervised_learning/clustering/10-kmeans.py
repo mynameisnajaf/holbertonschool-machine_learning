16c885fe5ef304471a57d7f6a59fd3568005bac2
mynameisnajaf <12692@holbertonstudents.com>
2026-05-24 04:40:51 +0400

    / 0-0 \

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -5,7 +5,7 @@ import sklearn.cluster
 
 def kmeans(X, k):
     """A function that does the trick"""
-    k_means = sklearn.cluster.KMeans    (n_clusters=k)
+    k_means = sklearn.cluster.KMeans(n_clusters=k)
     k_means.fit(X)
     clss = k_means.labels_
     C = k_means.cluster_centers_
