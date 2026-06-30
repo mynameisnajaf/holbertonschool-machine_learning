9c31657e8ae3943acfb0ad41bbf7db4e8d58777a
mynameisnajaf <12692@holbertonstudents.com>
2026-06-30 15:01:44 +0400

    / 0-0 \

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python3
 """A module that does the trick"""
-import gensim.models as Word2Vec
 
 
-def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5,
-                   cbow=True, iterations=5, seed=0, workers=1):
+def gensim_to_keras(model):
     """A function that does the trick"""
-    if cbow:
-        cbow_flag = 0
-    else:
-        cbow_flag = 1
-    model = Word2Vec(sentences=sentences,
-                     size=size,
-                     min_count=min_count,
-                     window=window,
-                     negative=negative,
-                     sg=cbow_flag,
-                     iter=iterations,
-                     seed=seed,
-                     workers=workers)
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=model.epochs)
-    return model
+    return model.wv.get_keras_embedding(train_embeddings=True)
