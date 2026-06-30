ca971d3f7a949dbe8b2af77e57c51bb31aff31bd
mynameisnajaf <12692@holbertonstudents.com>
2026-06-30 15:42:25 +0400

    / 0-0 \

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -7,21 +7,16 @@ import gensim
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
     """Creates and trains a FastText model."""
+    model = gensim.models.FastText(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=0 if cbow else 1,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
 
-    if cbow is True:
-        cbow_flag = 0
-    else:
-        cbow_flag = 1
-    model = gensim.models.FastText(sentences=sentences,
-                     vector_size=vector_size,
-                     min_count=min_count,
-                     window=window,
-                     negative=negative,
-                     sg=cbow_flag,
-                     epochs=epochs,
-                     seed=seed,
-                     workers=workers)
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=model.epochs)
     return model
