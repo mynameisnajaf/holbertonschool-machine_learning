72d4f9f47f984e0aef461f73f2b79c8445c8c72b
mynameisnajaf <12692@holbertonstudents.com>
2026-06-30 15:04:21 +0400

    / 0-0 \

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -10,10 +10,12 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
 
     model = gensim.models.Word2Vec(
         sentences=sentences,
+        vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=0 if cbow else 1,
+        epochs=epochs,
         seed=seed,
         workers=workers
     )
