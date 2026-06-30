19620745502dd12f894974e5f67d8ec6d1d76411
mynameisnajaf <12692@holbertonstudents.com>
2026-06-30 15:35:31 +0400

    / 0-0 \

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -12,7 +12,7 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
     model = gensim.models.FastText(sentences, min_count=min_count,
                                    epochs=epochs, vector_size=vector_size,
                                    window=window, negative=negative,
-                                   seed=seed, sg = 0 if cbow else 1, workers=workers)
+                                   seed=seed, sg=cbow, workers=workers)
     model.train(sentences, total_examples=model.corpus_count,
                 epochs=model.epochs)
 
