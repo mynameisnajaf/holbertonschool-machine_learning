eab9ec1d8d6f2e510b3b90eaf2ea17bbcf5659ab
mynameisnajaf <12692@holbertonstudents.com>
2026-06-30 15:20:19 +0400

    / 0-0 \

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -18,3 +18,10 @@ def gensim_to_keras(model):
     )
 
     return embedding
+from gensim.test.utils import common_texts
+word2vec_model = __import__('2-word2vec').word2vec_model
+gensim_to_keras = __import__('3-gensim_to_keras').gensim_to_keras
+
+print(common_texts[:2])
+w2v = word2vec_model(common_texts, min_count=1)
+print(gensim_to_keras(w2v))
