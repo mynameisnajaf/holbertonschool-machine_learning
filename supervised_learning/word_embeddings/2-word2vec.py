1f6b3c85c3716e482df72289f20a840b79b3b7e1
mynameisnajaf <12692@holbertonstudents.com>
2026-06-30 15:08:08 +0400

    / 0-0 \

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -8,16 +8,18 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Create and train a Word2Vec model."""
 
-    model = gensim.models.Word2Vec(
-        sentences=sentences,
-        vector_size=vector_size,
-        min_count=min_count,
-        window=window,
-        negative=negative,
-        sg=0 if cbow else 1,
-        epochs=epochs,
-        seed=seed,
-        workers=workers
-    )
-
+    if cbow is True:
+        cbow_flag = 0
+    else:
+        cbow_flag = 1
+    model = gensim.models.Word2Vec(sentences=sentences,
+                     min_count=min_count,
+                     window=window,
+                     negative=negative,
+                     sg=cbow_flag,
+                     seed=seed,
+                     workers=workers)
+    model.train(sentences,
+                total_examples=model.corpus_count,
+                epochs=model.epochs)
     return model
