516900373473f7f097d220c65e928782fd5440c3
mynameisnajaf <12692@holbertonstudents.com>
2026-07-06 15:43:11 +0400

    / 0-0 \

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -14,6 +14,7 @@ def positional_encoding(max_seq_len, dm):
             positional_encoding[pos, i + 1] = np.cos(get_angles(pos, i, dm))
     return positional_encoding
 
+
 def get_angles(pos, i, dm):
     """calculates the angles for a transformer"""
     angle_rates = 1 / (10000 ** (i / dm))
