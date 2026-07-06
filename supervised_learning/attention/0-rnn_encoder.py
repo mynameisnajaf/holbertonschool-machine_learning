3e207c62a8ccc45543100840df088a260d9e6b35
mynameisnajaf <12692@holbertonstudents.com>
2026-07-06 15:12:03 +0400

    / 0-0 \

---
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""RNN Encoder"""
+
+import tensorflow as tf
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """RNN Encoder class"""
+
+    def __init__(self, vocab, embedding, units, batch):
+        """Initialize RNN Encoder class"""
+        if type(vocab) is not int:
+            raise TypeError(
+                "vocab must be int representing the size of input vocabulary")
+        if type(embedding) is not int:
+            raise TypeError(
+                "embedding must be int representing dimensionality of vector")
+        if type(units) is not int:
+            raise TypeError(
+                "units must be int representing the number of hidden units")
+        if type(batch) is not int:
+            raise TypeError(
+                "batch must be int representing the batch size")
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                                                   output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(units=units,
+                                       return_state=True,
+                                       return_sequences=True,
+                                       recurrent_initializer="glorot_uniform")\
+
+    def initialize_hidden_state(self):
+        """Initialize hidden state"""
+        hidden_state = tf.zeros(shape=[self.batch, self.units])
+        return hidden_state
+
+    def call(self, x, initial):
+        """Call RNN encoder"""
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
+        return outputs, hidden
