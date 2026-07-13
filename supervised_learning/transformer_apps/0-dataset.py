d2f9ad285b276c3c1bc07092d4d22cde04e402f5
mynameisnajaf <12692@holbertonstudents.com>
2026-07-13 11:37:27 +0400

    / 0-0 \

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """A class that does the trick"""
+
+    def __init__(self):
+        """Constructor"""
+        self.data_train = tfds.load("ted_hrlr_translate/pt_to_en",
+                                    split="train",
+                                    as_supervised=True)
+        self.data_valid = tfds.load("ted_hrlr_translate/pt_to_en",
+                                    split="validation",
+                                    as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """Tokenize the dataset"""
+        SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
+        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data),
+            target_vocab_size=(2 ** 15))
+        tokenizer_en = SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data),
+            target_vocab_size=(2 ** 15))
+        return tokenizer_pt, tokenizer_en
