d35eb9597f90b168705a72f408a57a2781b715b8
mynameisnajaf <12692@holbertonstudents.com>
2026-08-11 15:25:10 +0400

    / 0-0 \

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import gymnasium as gym
+import numpy as np
+
+
+def q_init(env):
+    """A function that does the trick"""
+    n_states = env.observation_space.n
+    n_actions = env.action_space.n
+    q_table = np.zeros((n_states, n_actions))
+    return q_table
