eb049d06292f51d286a1d6b65642cafe09d4bcf0
mynameisnajaf <12692@holbertonstudents.com>
2026-08-11 15:29:43 +0400

    / 0-0 \

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A module that does the trick"""
+import numpy as np
+from kagglehub import env
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """A function that does the trick"""
+    p = np.random.uniform(0, 1)
+    if p < epsilon:
+        next_action = np.random.randint(0, Q.shape[1])
+    else:
+        next_action = np.argmax(Q[state])
+    return next_action
