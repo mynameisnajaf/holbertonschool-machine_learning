3dad8461ba170124f67c20c2a277d03b55c2bee2
mynameisnajaf <quluyevneceff@gmail.com>
2026-08-20 23:35:06 +0400

    / 0-0 \

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -7,29 +7,26 @@ import numpy as np
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
     """Performs the Monte Carlo algorithm."""
-    for ep in range(episodes):
+    for episode in range(episodes):
         state = env.reset()[0]
-        episode = []
+        episode_data = []
 
-        # Generate an episode
         for step in range(max_steps):
             action = policy(state)
-            next_state, reward, done, truncated, info = env.step(action)
-            episode.append((state, reward))
-            state = next_state
-            if done or truncated:
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            episode_data.append((state, reward))
+
+            if terminated or truncated:
                 break
+            state = next_state
 
-        episode = np.array(episode, dtype=int)
-
-        # Compute returns and update V (first-visit MC)
         G = 0
-        for t in reversed(range(len(episode))):
-            state_t, reward_t = episode[t]
-            G = reward_t + gamma * G
+        episode_data = np.array(episode_data, dtype=int)
 
-            # First-visit check
-            if state_t not in episode[:t, 0]:
-                V[state_t] = V[state_t] + alpha * (G - V[state_t])
+        for state, reward in reversed(episode_data):
+            G = reward + gamma * G
+
+            if state not in episode_data[:episode, 0]:
+                V[state] += alpha * (G - V[state])
 
     return V
