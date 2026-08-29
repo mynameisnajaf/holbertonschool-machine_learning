d6db43a35f3f22818547ca05f85e17348c452851
mynameisnajaf <quluyevneceff@gmail.com>
2026-08-29 15:11:41 +0400

    / 0-0 \

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -7,16 +7,18 @@ def policy(matrix, weight):
     """Policy function"""
     z = np.matmul(matrix, weight)
 
-    # Softmax
     exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
     return exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
 def policy_gradient(state, weight):
     """Computes the Monte-Carlo policy gradient based on a state and weight matrix."""
-    Policy = policy(state, weight)
-    action = np.random.choice(len(Policy[0]), p=Policy[0])
-    s = Policy.reshape(-1, 1)
-    softmax = (np.diagflat(s) - np.dot(s, s.T))[action, :]
-    dlog = softmax / Policy[0, action]
-    gradient = state.T.dot(dlog[None, :])
+    p = policy(state, weight)
+
+    action = np.random.choice(p.shape[1], p=p[0])
+
+    one_hot = np.zeros(p.shape[1])
+    one_hot[action] = 1
+
+    gradient = np.outer(state[0], one_hot - p[0])
+
     return action, gradient
